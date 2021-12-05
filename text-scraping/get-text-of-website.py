ffbd732d92a1f8cc2cfdab38b8e2de1280f35758
Kokostino <56522863+Kokostino@users.noreply.github.com>
2021-12-05 11:41:11 +0100

    scraped text

--- text-scraping/get-text-of-website.py
+++ text-scraping/get-text-of-website.py
@@ -2,43 +2,38 @@ from selenium import webdriver
 import os
 import time
 
-os.chdir('C:\\Users\\dherschmann\\Documents\\GitHub\\Instagram-Automation\\text-scraping')
 file_path = os.getcwd()
 driver = webdriver.Chrome(file_path+'/chromedriver.exe')
 
-def log_in(usrnme, psswrd):
-    
-    driver.get("https://www.instagram.com")
-    time.sleep(2)
-    
-    cookie_button = driver.find_element_by_xpath("//button[text()='Alle annehmen']")
-    cookie_button.click()
-    
-    time.sleep(4)
-    username_input = driver.find_element_by_css_selector("input[name='username']")
-    password_input = driver.find_element_by_css_selector("input[name='password']")
-    
-    username_input.send_keys(usrnme)
-    password_input.send_keys(psswrd)
-    
-    login_button =driver.find_element_by_xpath("//button[@type='submit']")
-    login_button.click()
-    
-    time.sleep(5)
-    
-    save_login_info_button= driver.find_element_by_xpath("//button[text()='Jetzt nicht']")
-    save_login_info_button.click()
-    time.sleep(7)
-    notification_button= driver.find_element_by_xpath("//button[text()='Jetzt nicht']")
-    notification_button.click()
+driver.get("https://www.instagram.com")
+time.sleep(2)
 
-def insta_comment(usrnme, psswrd, urls):
-    log_in(usrnme, psswrd)
+cookie_button = driver.find_element_by_xpath("//button[text()='Alle annehmen']")
+cookie_button.click()
+
+time.sleep(4)
+username_input = driver.find_element_by_css_selector("input[name='username']")
+password_input = driver.find_element_by_css_selector("input[name='password']")
+    
+username_input.send_keys(USERNAME)
+password_input.send_keys(PASSWORT)
+    
+login_button =driver.find_element_by_xpath("//button[@type='submit']")
+login_button.click()
+    
+time.sleep(3)
+    
+save_login_info_button= driver.find_element_by_xpath("//button[text()='Jetzt nicht']")
+save_login_info_button.click()
+time.sleep(4)
+notification_button= driver.find_element_by_xpath("//button[text()='Jetzt nicht']")
+notification_button.click()
+time.sleep(7)
+
+def insta_comment(urls):
     post_comments = []
     for link in urls:
         driver.get(link)
-#        cookie_button = driver.find_element_by_xpath("//button[text()='Accept All']")
-#        cookie_button.click()
         try:
             comment = driver.find_elements_by_xpath('//div[@class="C4VMK"]//span')[2].text
             post_comments.append(comment)
@@ -47,13 +42,6 @@ def insta_comment(usrnme, psswrd, urls):
         time.sleep(10)
     return post_comments
 
-def write_to_txt_file(liste,file_name):
-    
-    textfile = open(file_name, "a+")
-    for element in liste:
-        textfile.write(element + "\n")
-    textfile.close()
-    
 def read_from_file(file):   
     site_list=[]
     with open(file) as f:
@@ -61,8 +49,29 @@ def read_from_file(file):
             li = [l.strip() for l in line.split('\n')]
             site_list.append(li[0])
         return site_list
+
+def write_to_txt_file(list,file_name):
+    textfile = open(file_name, "a+", encoding="utf-8")
+    for element in list:
+        textfile.write(element + "\n")
+    textfile.close()
     
 post_links = read_from_file("websites-shaaz.txt")
+full_text=insta_comment(post_links)
+write_to_txt_file(full_text,'shaaz.txt')
 
-full_text=insta_comment(MYUSERNAME, MYPASSWORD, post_links)
-write_to_txt_file(full_text,"shaaz-text.txt")
+
+
+
+import re
+def write_to_txt_file_other(liste,file_name):
+    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags=re.UNICODE)
+    textfile = open(file_name, "w+")
+    for element in liste:
+        textfile.write(emoji_pattern.sub(r'', element) + "\n")
+    textfile.close()
